2119cd84b4e9d7477993cd27d3b45775b0413348
Guy Chauliac <guy.chauliac@axa.be>
2022-12-01 15:52:58 +0100

    add capability to add header to DefaultRemoteRepositoryRequestBuilder

--- src/main/java/org/maxxq/maven/repository/DefaultRemoteRepositoryRequestBuilder.java
+++ src/main/java/org/maxxq/maven/repository/DefaultRemoteRepositoryRequestBuilder.java
@@ -1,14 +1,23 @@
 package org.maxxq.maven.repository;
 
+import okhttp3.Headers;
 import okhttp3.Request;
 
 public class DefaultRemoteRepositoryRequestBuilder implements IRemoteRepositoryRequestBuilder {
+    private final Headers.Builder headerBuilder = new Headers.Builder();
 
     @Override
     public Request apply( String endPoint ) {
         return new Request.Builder()
             .url( endPoint )
+            .headers( headerBuilder.build() )
             .build();
     }
 
+    public DefaultRemoteRepositoryRequestBuilder addHeader( String key, String value ) {
+        headerBuilder.add( key, value );
+        return this;
+    }
+
 }
+
